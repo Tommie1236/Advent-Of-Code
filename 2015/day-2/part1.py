c776c04026da7fed7a6100ce437064af2a8307ae
tommie1236 <timooosterom@gmail.com>
2023-01-05 19:56:52 +0100

    moved the var total to the top

--- 2015/day-2/part1.py
+++ 2015/day-2/part1.py
@@ -2,7 +2,8 @@ from data_format import data as boxes
 import pprint
 p = pprint.PrettyPrinter()
 
-#boxes = [[2, 3, 4], [1, 1, 10]] 
+#boxes = [[2, 3, 4], [1, 1, 10]] # This is test data to check if the script work 
+total = 0
 
 # function to calculate the amount of paper needed for a box
 def calculate_paper(sizes):
@@ -11,7 +12,6 @@ def calculate_paper(sizes):
 	slack = min(l*w, w*h, h*l)
 	return paper + slack
 
-total = 0
 
 for box in boxes:
 	total += calculate_paper(box)
